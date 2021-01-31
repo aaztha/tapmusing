f7474697e81bd4de3d66ed84383d30e1fd336a92
Aastha Sharma <51191976+aaztha@users.noreply.github.com>
2021-01-31 15:23:12 +0530

    Add files via upload

---
+++ beatmaker-js/index.js
@@ -0,0 +1,38 @@
+window.addEventListener("load", () => {
+  const sounds = document.querySelectorAll(".sound");
+  const pads = document.querySelectorAll(".pads div");
+  const visual = document.querySelector(".visual");
+  const colors = [
+    "#00E833",
+    "#d36060",
+    "#c060d3",
+    "red",
+    "#606bd3",
+    "#60c2d3",
+    "#0367FC",
+    "#FC0383",
+    "#FC4E03",
+    "#9BFC03",
+    "#03FCDC",
+    "#FF628F",
+  ];
+
+  pads.forEach((pad, index) => {
+    pad.addEventListener("click", function() {
+      sounds[index].currentTime = 0;
+      sounds[index].play();
+      createBubble(index);
+    });
+  });
+
+  const createBubble = index => {
+    //Create bubbles
+    const bubble = document.createElement("div");
+    visual.appendChild(bubble);
+    bubble.style.backgroundColor = colors[index];
+    bubble.style.animation = `jump 1s ease`;
+    bubble.addEventListener("animationend", function() {
+      visual.removeChild(this);
+    });
+  };
+});
